e2ee965dbf2d9987c5992ede795797d2f871137e
tech-rushil <rushil.rushil.gupta@gmail.com>
2021-09-19 20:29:39 +0530

    video modal added

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 import testimonialData from "./testimonialData";
+import Modal from "react-modal";
+import ReactPlayer from "react-player";
 
 const settings = {
     dots: false,
@@ -13,14 +15,75 @@ const settings = {
     adaptiveHeight: true,
 };
 
-const VideoItem = ({ image }) => {
+const VideoModal = ({ video_link, modalOpen, setmodalOpen }) => {
+    const customStyles = {
+        content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 1000,
+            // backgroundColor: "rgba(0, 0, 0, 0.45)",
+            borderRadius: "unset",
+            border: "unset",
+            background: "tarnsparent",
+        },
+    };
+
+    return (
+        <>
+            <Modal isOpen={modalOpen} onRequestClose={() => setmodalOpen(false)} style={customStyles}>
+                <div className="video-play-container f-d f-h-c f-v-c">
+                    <ReactPlayer
+                        className="intro-video-player"
+                        url={video_link}
+                        playing
+                        controls
+                        config={{
+                            file: {
+                                attributes: {
+                                    controlsList: "nodownload",
+                                    disablePictureInPicture: true,
+                                },
+                            },
+                        }}
+                    />
+                    <div className="close-btn c-pointer" onClick={() => setmodalOpen(false)}></div>
+                </div>
+            </Modal>
+            <style jsx>
+                {`
+                    .video-play-container {
+                        padding: 64px;
+                        position: relative;
+                    }
+
+                    .close-btn {
+                        position: absolute;
+                        width: 80px;
+                        height: 80px;
+                        background-color: rgba(0, 0, 0, 0.5);
+                        border-radius: 50%;
+                        top: -16px;
+                        right: -16px;
+                        z-index: 10;
+                    }
+                `}
+            </style>
+        </>
+    );
+};
+
+const VideoItem = ({ image, video_link, handleSetVideo }) => {
     return (
         <>
             <div className="carousel-item">
                 <div className="carousel-image">
                     <Image src={image} layout={"fill"} objectFit={"cover"} />
                 </div>
-                <div className="play-btn-container f-d f-h-c ">
+                <div className="play-btn-container f-d f-h-c c-pointer" onClick={() => handleSetVideo(video_link)}>
                     <div className="play-btn c-pointer  f-d f-h-c f-v-c">
                         <div className="play-icon">
                             <Image src={"/icons/play.svg"} layout={"fill"} objectFit={"contain"} />
@@ -66,6 +129,8 @@ const VideoItem = ({ image }) => {
 
 const Testimonials = () => {
     const carouselRef = useRef(null);
+    const [video, setvideo] = useState("");
+    const [modalOpen, setmodalOpen] = useState(false);
 
     const handleCarouselNav = (type = "prev") => {
         if (carouselRef) {
@@ -83,8 +148,15 @@ const Testimonials = () => {
         }
     };
 
+    const setVideoHandler = (video_link) => {
+        setvideo(video_link);
+        setmodalOpen(true);
+    };
+
     const renderCarouselItem = (list) => {
-        return list.map(({ image, video_link }, idx) => <VideoItem image={image} key={`VID-{idx}`} />);
+        return list.map(({ image, video_link }, idx) => (
+            <VideoItem image={image} key={`VID-{idx}`} video_link={video_link} handleSetVideo={setVideoHandler} />
+        ));
     };
 
     return (
@@ -113,6 +185,7 @@ const Testimonials = () => {
                             </div>
                         </div>
                     </div>
+                    <VideoModal modalOpen={modalOpen} video_link={video} setmodalOpen={setmodalOpen} />
                 </div>
             </section>
             <style jsx>
